7b7358cecf752d72ff848a4b0c6fa4e40d06dbab
gioretikto <gioresta87@gmail.com>
2021-08-01 10:17:35 +0200

    update all

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-# litos A texteditor written in C with GTK3+ and gtksourceview-4
+# litos (v. 1.0 Experimental version)
 
-#compile with make
+litos is a simple text editor for the [Xfce](https://www.xfce.org) desktop environment.
+
+**litos** aims to be an easy-to-use and fast editor. Our target is an
+editor for quickly editing text files, providing shortcuts for inserting HTML tags. I try to use the latest
+GTK features available, which means that if GTK adds something new in a major
+release that is useful for the editor, we will likely bump the GTK dependency
+and integrate this new feature in **litos**.
+
+### Required Packages 
+
+Mousepad depends on the following packages:
+
+* [GTK](https://www.gtk.org) >=3.20.0
+* [GLib](https://wiki.gnome.org/Projects/GLib) >=2.42.0
+* [GtkSourceView](https://wiki.gnome.org/Projects/GtkSourceView) >=4.0
+
+### Installation
+
+From source code repository: 
+
+    % cd litos
+    % make
+
+###Authors
+
+Giovanni Resta - giovannirestadev@gmail.com
+
+### Reporting Bugs
+
+giovannirestadev@gmail.com
--- gui.c
+++ gui.c
@@ -4,8 +4,16 @@
 #include <gtksourceview/gtksource.h>
 #include <glib.h>
 
+#define CURRENT_PAGE -2
+
 GtkWidget *window;
+GtkNotebook *notebook;
 GtkSourceBuffer *buffer;
+
+void open_file(char *filename);
+void open_dialog();
+void save_dialog();
+void menu_newtab();
 
 static void
 quit_activated (GSimpleAction *action,
@@ -13,6 +21,53 @@ quit_activated (GSimpleAction *action,
                 gpointer       app)
 {
   g_application_quit (G_APPLICATION (app));
+}
+
+const GActionEntry app_entries[] = {
+    {"new", menu_newtab, NULL, NULL, NULL, {0,0,0}},
+	{"open", open_dialog, NULL, NULL, NULL, {0,0,0}},
+	{"save", save_dialog, NULL, NULL, NULL, {0,0,0}},
+    {"quit", quit_activated, NULL, NULL, NULL, {0,0,0}}
+};
+
+struct {
+  const gchar *action;
+  const gchar *accels[2];
+} action_accels[] = {
+  { "app.new", { "<Control>n", NULL} },
+  { "app.open", { "<Control>o", NULL} },
+  { "app.quit", { "<Control>q", NULL} },
+  { "app.save", { "<Control>s", NULL} },
+  { "app.saveas", { "<Shift><Control>s", NULL} },
+  { "win.close", { "<Control>w", NULL} },
+  { "win.cut", { "<Control>x", NULL} },
+  { "win.copy", { "<Control>c", NULL} },
+  { "win.paste", { "<Control>v", NULL} },
+  { "win.selectall", { "<Control>a", NULL} }
+};
+
+GList* get_tabbox_children(GtkNotebook *tabnotebook, const gint page){
+    return gtk_container_get_children(GTK_CONTAINER(gtk_notebook_get_nth_page(
+      tabnotebook,
+      page
+    )));
+}
+
+GtkSourceView* tab_get_sourceview(int page)
+{
+
+    if (page == CURRENT_PAGE){
+        page = gtk_notebook_get_current_page(notebook);
+    }
+    
+    g_print("Page: %d\n",page);
+ 
+    GList *children = get_tabbox_children(
+      notebook,
+      page
+    );
+
+    return GTK_SOURCE_VIEW(gtk_bin_get_child(GTK_BIN(g_list_nth_data(children, 0))));
 }
 
 void open_file(char *filename)
@@ -26,8 +81,12 @@ void open_file(char *filename)
     	g_error("error opening file: %s\n",error && error->message ? error->message : "No Detail");
         return;
     }
+    
+    GtkSourceView *source_view = tab_get_sourceview(gtk_notebook_get_current_page(notebook));
 
-    gtk_text_buffer_set_text(GTK_TEXT_BUFFER(buffer), contents, -1);
+	GtkTextBuffer *current_buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW(source_view));
+	
+	gtk_text_buffer_set_text(GTK_TEXT_BUFFER(current_buffer), contents, -1);
 }
 
 void open_dialog()
@@ -56,19 +115,24 @@ void open_dialog()
 		open_file (filename);
 		g_free (filename);
 	  }
-	  
+
 	 gtk_widget_destroy (dialog);		  
 }
 
 void save_file(GtkFileChooser *chooser)
 {
-		GFile *loc = gtk_file_chooser_get_file(chooser);
-		GtkTextIter begin, end;
-		gtk_text_buffer_get_start_iter(GTK_TEXT_BUFFER(buffer), &begin);
-		gtk_text_buffer_get_end_iter(GTK_TEXT_BUFFER(buffer), &end);
-		char* contents = gtk_text_buffer_get_text(GTK_TEXT_BUFFER(buffer), &begin, &end, 0);
-		g_file_replace_contents(loc, contents, strlen(contents), NULL, TRUE, G_FILE_CREATE_NONE, NULL, NULL, NULL);
-		g_object_unref(loc);
+	GFile *loc = gtk_file_chooser_get_file(chooser);
+	GtkTextIter begin, end;
+	
+	GtkTextView *text_view = GTK_TEXT_VIEW(tab_get_sourceview(CURRENT_PAGE));
+
+	GtkTextBuffer *current_buffer = gtk_text_view_get_buffer (text_view);
+
+	gtk_text_buffer_get_start_iter(current_buffer, &begin);
+	gtk_text_buffer_get_end_iter(current_buffer, &end);
+	char* contents = gtk_text_buffer_get_text(current_buffer, &begin, &end, 0);
+	g_file_replace_contents(loc, contents, strlen(contents), NULL, TRUE, G_FILE_CREATE_NONE, NULL, NULL, NULL);
+	g_object_unref(loc);
 }
 
 void save_dialog()
@@ -90,13 +154,6 @@ void save_dialog()
 
 	gtk_file_chooser_set_do_overwrite_confirmation (chooser, TRUE);
 
-	/*if (user_edited_a_new_document)
-	  gtk_file_chooser_set_current_name (chooser,
-		                                 _("Untitled document"));
-	else
-	  gtk_file_chooser_set_filename (chooser,
-		                             existing_filename);*/
-
 	res = gtk_dialog_run (GTK_DIALOG (dialog));
 	if (res == GTK_RESPONSE_ACCEPT)
 	  {
@@ -115,59 +172,16 @@ void action_quit()
 	gtk_main_quit();
 }
 
-const GActionEntry app_entries[] = {
-  //{"new", new_activated, NULL, NULL, NULL, {0,0,0}},
-	{"open", open_dialog, NULL, NULL, NULL, {0,0,0}},
-	{"save", save_dialog, NULL, NULL, NULL, {0,0,0}},
-    {"quit", quit_activated, NULL, NULL, NULL, {0,0,0}}
-};
-
-struct {
-  const gchar *action;
-  const gchar *accels[2];
-} action_accels[] = {
-  { "app.new", { "<Control>n", NULL} },
-  { "app.open", { "<Control>o", NULL} },
-  { "app.quit", { "<Control>q", NULL} },
-  { "app.save", { "<Control>s", NULL} },
-  { "app.saveas", { "<Shift><Control>s", NULL} },
-  { "win.close", { "<Control>w", NULL} },
-  { "win.cut", { "<Control>x", NULL} },
-  { "win.copy", { "<Control>c", NULL} },
-  { "win.paste", { "<Control>v", NULL} },
-  { "win.selectall", { "<Control>a", NULL} }
-};
-
-void new_file()
+GtkWidget* new_sourceview()
 {
-	typedef struct App 
-	{
-		GtkWidget *source_view;
-		GtkWidget *search_entry;
-	}App;
-
-	GtkWidget *swindow;
-	
-	App AppSource;
-
-	swindow = gtk_scrolled_window_new (NULL, NULL);
-  
-	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (swindow),
-	GTK_POLICY_AUTOMATIC,
-	GTK_POLICY_AUTOMATIC);
-
-	gtk_widget_set_vexpand (swindow, TRUE);
-  
-	gtk_container_add(GTK_CONTAINER (window), swindow);
-
-	//GtkSourceBuffer *buffer;
+	GtkWidget *source_view;
 
     buffer = gtk_source_buffer_new (NULL);
 	gtk_source_buffer_set_style_scheme (buffer,
 	gtk_source_style_scheme_manager_get_scheme (
 	gtk_source_style_scheme_manager_get_default (), "solarized-dark"));
 
-	AppSource.source_view = gtk_source_view_new_with_buffer (buffer);	
+	source_view = gtk_source_view_new_with_buffer (buffer);
 
 	GFile *location = g_file_new_for_path ("/home/gidna/prova");
 
@@ -175,11 +189,48 @@ void new_file()
 
 	gtk_source_file_set_location (file, location);
 
-    gtk_container_add (GTK_CONTAINER (swindow), AppSource.source_view);
-
   	gtk_source_file_saver_new (buffer, file);
 
-    gtk_source_view_set_show_line_numbers (GTK_SOURCE_VIEW((AppSource.source_view)), TRUE);
+    gtk_source_view_set_show_line_numbers (GTK_SOURCE_VIEW((source_view)), TRUE);
+
+    return source_view;
+}
+
+void menu_newtab(void)
+{
+    GtkWidget *scrolled_window;
+    GtkWidget *tabbox;
+    GtkWidget *source_view = new_sourceview();
+
+    tabbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 1);
+    
+    scrolled_window = gtk_scrolled_window_new (NULL, NULL);
+    
+    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window),
+	GTK_POLICY_AUTOMATIC,
+	GTK_POLICY_AUTOMATIC);
+
+	gtk_widget_set_hexpand (scrolled_window, TRUE);
+
+	gtk_container_add(GTK_CONTAINER(scrolled_window), source_view);
+
+	gtk_container_add (GTK_CONTAINER(tabbox), scrolled_window);
+
+    gtk_notebook_append_page_menu(
+      notebook,
+      tabbox,
+      gtk_label_new("UNSAVED"),
+      gtk_label_new("UNSAVED")
+    );
+
+    gtk_notebook_set_current_page(
+      notebook,
+      gtk_notebook_get_n_pages(notebook) - 1
+    );
+    
+    gtk_notebook_set_tab_reorderable(notebook, tabbox, TRUE);
+    
+	gtk_widget_show_all(GTK_WIDGET(tabbox));
 }
 
 void create_menu (GtkApplication *app)
@@ -189,16 +240,17 @@ void create_menu (GtkApplication *app)
 		
 		m = g_menu_new ();
 		fm = g_menu_new();
+		g_menu_append(fm, "New	", "app.new");
 		g_menu_append(fm, "Open	", "app.open");
 		g_menu_append(fm, "Save	", "app.save");
 		g_menu_append(fm, "Close Window	", "app.quit");
 		g_menu_append_submenu (m, "File", G_MENU_MODEL(fm));
 		gtk_application_set_menubar(GTK_APPLICATION(app), G_MENU_MODEL(m));
 
-		  g_action_map_add_action_entries(G_ACTION_MAP(app), app_entries, G_N_ELEMENTS(app_entries), app);
+		g_action_map_add_action_entries(G_ACTION_MAP(app), app_entries, G_N_ELEMENTS(app_entries), app);
   			for (i = 0; i < G_N_ELEMENTS(action_accels); i++)
 				gtk_application_set_accels_for_action(GTK_APPLICATION(app), action_accels[i].action, action_accels[i].accels);
-				
+
 		g_object_unref (m);
 }
 
@@ -209,14 +261,18 @@ activate (GtkApplication* app,
 	window = gtk_application_window_new (app);
 	gtk_window_set_title (GTK_WINDOW (window), "Litos");
 	gtk_window_maximize (GTK_WINDOW(window));
-  
+
 	user_data = "well";
-  
-    g_print ("Hello again - %s done\n", (char *) user_data);    
-    
+
+    g_print ("Hello again - %s done\n", (char *) user_data);
+
+    notebook = GTK_NOTEBOOK(gtk_notebook_new());
+
+    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(notebook));
+
     create_menu(app);
-    
-    new_file();
-    
+
+    menu_newtab();
+
     gtk_widget_show_all (window);
 }
