211334a253527a0b00ccaf4bc1869b203558c6d1
cadmio <cadmio@mywifi.mywifi>
2022-12-13 12:41:59 +0100

    update all

--- spell.c
+++ spell.c
@@ -10,60 +10,70 @@ void spellCheck (GtkWidget *button, struct lit *litos)
 	(void)button;
 
 	AspellConfig * spell_config = new_aspell_config();
+
 	aspell_config_replace(spell_config, "lang", "en_US");
 
 	AspellCanHaveError * possible_err = new_aspell_speller(spell_config);
 	AspellSpeller * spell_checker = 0;
 
 	if (aspell_error_number(possible_err) != 0)
-	  puts(aspell_error_message(possible_err));
+		puts(aspell_error_message(possible_err));
 	else
-	  spell_checker = to_aspell_speller(possible_err);
+		spell_checker = to_aspell_speller(possible_err);
 
 	int correct;
 
-    gint idx = 0;
-    GtkTextIter start, end;
+	gint idx = 0;
+	GtkTextIter start, end;
 	GtkTextBuffer *buffer = get_current_buffer(litos);
 
-    gtk_text_buffer_begin_user_action (buffer); /* begin user action */
+	gtk_text_buffer_begin_user_action (buffer); /* begin user action */
 
-    gtk_text_buffer_get_start_iter (buffer, &start);
+	gtk_text_buffer_get_start_iter (buffer, &start);
 
-    while (!gtk_text_iter_starts_word (&start))
-        gtk_text_iter_forward_char (&start);
+	while (!gtk_text_iter_starts_word (&start))
+		gtk_text_iter_forward_char (&start);
 
-    end = start;
+	end = start;
 
-    while (gtk_text_iter_forward_word_end (&end)) {
-        const gchar *word = gtk_text_buffer_get_text (buffer,
+	while (gtk_text_iter_forward_word_end (&end))
+	{
+		const gchar *word = gtk_text_buffer_get_text (buffer,
                                             &start, &end, FALSE);
-        const gchar *p = word;
 
-        for (; *p; p++) {}
+		g_print ("word is: %s\n", word);
 
-        g_print ("[%4d] : %s\n", idx++, word);
+		const gchar *p = word;
 
-		correct = aspell_speller_check(spell_checker, word, (int)strlen(word));
-
-		if(!correct)
+		if(p[0] == '<')			/* if word contains a tag skip it */
 		{
-        	g_print ("Error on: %s\n", word);
-			highlightWord(litos, word);
+			while(*p != '>')
+				p++;
 		}
 
-        start = end;
+		g_print ("[%4d] : %s\n", idx++, p);
 
-        for (;;) {
-            if (!gtk_text_iter_forward_char (&start))
-                goto wordseldone;
-            if (gtk_text_iter_starts_word (&start))
-                break;
-        }
+		correct = aspell_speller_check(spell_checker, p, (int)strlen(p));
 
-        end = start;    /* not 100% needed, fwd_word_end will jumpt to next end */
-    }
-    wordseldone:;
+		if (!correct)
+		{
+			g_print ("Error on: %s\n", p);
+			highlightWord(litos, p);
+		}
 
-    gtk_text_buffer_end_user_action (buffer);   /* end user action */
+		start = end;
+
+		for (;;)
+		{
+			if (!gtk_text_iter_forward_char (&start))
+				goto wordsdone;
+			if (gtk_text_iter_starts_word (&start))
+				break;
+		}
+	}
+
+	wordsdone:;
+
+	gtk_text_buffer_end_user_action (buffer);   /* end user action */
+	delete_aspell_speller(spell_checker);
 }
